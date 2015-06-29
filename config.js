9488d2250dff1f154ab688e70e31ecc2b8f777ea
gefangshuai <gefangshuai@163.com>
2015-06-29 17:33:16 +0800

    project init

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by gefan on 2015/6/29.
+ */
+var site = {
+    title: 'NBlog',
+    author: 'gefangshuai',
+    email: 'gefangshuai@163.com',
+    copyright: new Date().getYear
+};
+module.exports.site = site;
